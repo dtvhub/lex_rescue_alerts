fadcf2043b901dcb889e35ed2b8493fc26c6d6dc
dtvhub <100465368+dtvhub@users.noreply.github.com>
2026-04-11 12:58:42 -0400

    Update cameras.js

--- map/js/cameras.js
+++ map/js/cameras.js
@@ -1,8 +1,27 @@
-L.geoJSON(cameraLayer, {
+// -----------------------------------------------------
+//  CAMERAS LAYER FOR BEACON MAP
+// -----------------------------------------------------
+
+// Custom camera icon
+const cameraIcon = L.icon({
+  iconUrl: '../assets/images/icons/camera.png',   // relative to /map/js/
+  iconSize: [28, 28],      // display size on map
+  iconAnchor: [14, 28],    // point of the icon that touches the map
+  popupAnchor: [0, -28]    // popup position relative to icon
+});
+
+// Create the cameras layer
+const cameras = L.geoJSON(cameraLayer, {
+  pointToLayer: (feature, latlng) => {
+    return L.marker(latlng, { icon: cameraIcon });
+  },
   onEachFeature: (feature, layer) => {
     layer.bindPopup(`
       <strong>${feature.properties.name}</strong><br>
       <a href="${feature.properties.url}" target="_blank">Open Camera</a>
     `);
   }
-}).addTo(map);
+});
+
+// Add to map
+cameras.addTo(map);
